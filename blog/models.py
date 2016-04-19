e6fd072107724f51809edf931eb78054d9c0d9cf
gwilku <grzesiek.wilczek@gmail.com>
2016-04-19 20:51:56 +0200

    Dodany model szkolenia

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,24 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Kurs(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    program = models.TextField()
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    start_date = models.DateTimeField(
+        blank=True, null=True)
+    end_date = models.DateTimeField(
+        blank=True, null=True)
+    
+    class Meta:
+		verbose_name_plural = "Kursy"
+    
+    def publish(self):
+        self.save()
+        
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .models import Post
+from .models import Post, Kurs
 
 # Create your views here.
 def post_list(request):
@@ -10,3 +10,7 @@ def post_list(request):
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post': post})
+	
+def szkolenia(request):
+	szkolenia = Kurs.objects.all()
+	return render(request, 'pages/szkolenia.html', {'szkolenia': szkolenia})
